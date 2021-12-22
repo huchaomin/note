cc00d3bd04031874ac3fbcb9912d6849419790ad
huchaomin <iisa_peter@163.com>
2021-12-22 16:37:10 +0800

    添加pwa功能

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -17,9 +17,8 @@ module.exports = {
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-    ['link', { rel: 'apple-touch-icon', href: '/icons/icon-152x152.png' }],
-    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
-    ['meta', { name: 'msapplication-TileImage', content: '/icons/icon-144x144.png' }],
+    ['link', { rel: 'apple-touch-icon', href: '/icons/apple-icon-152x152-seochecker-manifest-222.png' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/apple-icon-144x144-seochecker-manifest-222.png' }],
     ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
   ],
   markdown: {

2e92f4698237305ad5ab1eb126358c3a051edfc3
huchaomin <iisa_peter@163.com>
2022-02-19 18:31:18 +0800

    调整移动端滚动

--- src/.vuepress/theme/components/NavigationDrawer.vue
+++ src/.vuepress/theme/components/NavigationDrawer.vue
@@ -64,10 +64,26 @@ export default {
       active: [],
     }
   },
+  computed: {
+    noScroll () {
+      return this.$vuetify.breakpoint.mobile && this.show
+    },
+  },
   watch: {
     '$page': {
       handler () {
         this.focusItem()
+      },
+      immediate: true,
+    },
+    'noScroll': {
+      handler (value) {
+        const classList = document.querySelector('html').classList
+        if (value) {
+          !classList.contains('noScroll') && classList.add('noScroll')
+        } else {
+          classList.contains('noScroll') && classList.remove('noScroll')
+        }
       },
       immediate: true,
     },

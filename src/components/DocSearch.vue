54603d52bae35a7cc1c3decc6402d4678ea4a7f6
huchaomin <iisa_peter@163.com>
2023-02-08 11:48:06 +0800

    暂未完成 docsearch

--- src/components/DocSearch.vue
+++ src/components/DocSearch.vue
@@ -2,56 +2,32 @@
 import '@docsearch/css';
 import docsearch from '@docsearch/js';
 
-// const props = defineProps<{
-//   algolia: DefaultTheme.AlgoliaSearchOptions
-// }>()
-
-const router = useRouter();
-const route = useRoute();
-
-function initialize() {
-  const options = {
+const { origin } = window.location;
+onMounted(() => {
+  docsearch({
     container: '#docsearch',
-    appId: 'C9LSIOZAYC',
-    apiKey: '1172d6fa70c5b4fd88e3b3908d0e2ee1',
+    appId: 'VAOT8ETVIP',
+    apiKey: '0ef1096cfa74a3fad269ba4af7a5612c',
     indexName: 'mulinzi_note',
-
-    // navigator: {
-    //   navigate({ itemUrl }) {
-    //     debugger;
-    //     const { pathname: hitPathname } = new URL(
-    //       window.location.origin + itemUrl,
-    //     );
-
-    //     // router doesn't handle same-page navigation so we use the native
-    //     // browser location API for anchor navigation
-    //     if (route.path === hitPathname) {
-    //       window.location.assign(window.location.origin + itemUrl);
-    //     } else {
-    //       router.go(itemUrl);
-    //     }
-    //   },
-    // },
-
-    // // @ts-expect-error vue-tsc thinks this should return Vue JSX but it returns the required React one
-    // hitComponent({ hit, children }) {
-    //   return {
-    //     __v: null,
-    //     type: 'a',
-    //     ref: undefined,
-    //     constructor: undefined,
-    //     key: undefined,
-    //     props: { href: hit.url, children },
-    //   };
-    // },
-  };
-  onMounted(() => {
-    docsearch(options);
+    placeholder: '搜索笔记...',
+    startUrls: [origin], // TODO
+    sitemaps: [`${origin}/sitemap.xml`],
+    transformItems(items) {
+      console.log(items);
+      return items.map((item) => ({
+        ...item,
+        content: item.content.toUpperCase(),
+      }));
+    },
   });
-}
-initialize();
+});
 </script>
 
 <template>
   <div id="docsearch"></div>
 </template>
+<style lang="scss">
+  .DocSearch-Container{
+    z-index: 9999;
+  }
+</style>
